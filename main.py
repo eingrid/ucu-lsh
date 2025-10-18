c4f3c485bbe94af5775a272426117f6099d06e18
Nazar Andrushko <zikzak282@gmail.com>
2025-10-18 19:41:25 +0300

    implement lsh

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from ucu!")
+from src.lsh import LSH
+import csv 
 
 
-if __name__ == "__main__":
-    main()
+
+with open("topviews-2019_04.csv",) as f:
+    reader = csv.DictReader(f, delimiter=',')
+    rows = (list(reader))
+
+page_names = [row['Page'] for row in rows]
+
+lsh = LSH(band_size=10)
+
+lsh.fit(page_names)
+
+result = lsh.find_candidates()
+
+print(result)
+
+for x_id,y_id in result:
+    print("-"*50)
+    print(page_names[x_id])
+    print(page_names[y_id])
+    print("-"*50)
--- src/lsh.py
+++ src/lsh.py
@@ -4,20 +4,26 @@ import random
 
 class LSH():
 
-    def __init__(self, ):
-        pass
+    def __init__(self, signature_length=100, band_size = 2):
+        self.signature_length = signature_length
+        self.band_size = band_size
+        if signature_length % band_size != 0:
+            raise ValueError("Signature length should be divisible by band_size")
+        self.num_bands = signature_length // band_size
+        
 
 
-
-    def fit(self, corpus : list[str]):
-        shingles = self.create_shingles(corpus)
+    def fit(self, samples : list[str]):
+        shingles = self.create_shingles(samples)
         vocab, inv_vocab = self.create_vocab(shingles)
-
-        pass
-
-    def create_shingles(self, corpus:list[str], shingle_length: int = 2) -> list[set[str]]:
+        one_hot_encoded_samples = self.one_hot_encode(shingles, vocab)
+        self.minhashes = self.create_minhashes(len(vocab), self.signature_length)
+        signatures = self.compress_to_signatures(one_hot_encoded_samples)
+        self.hash_tables = self.create_bands_and_hash(signatures)
+    
+    def create_shingles(self, samples:list[str], shingle_length: int = 2) -> list[set[str]]:
         shingles = []
-        for text in corpus:
+        for text in samples:
             sample_shingles = set()
             for start_pos in range(len(text) - shingle_length):
                 sample_shingles.add(text[start_pos:shingle_length+start_pos])
@@ -25,8 +31,8 @@ class LSH():
         return shingles
 
 
-    def create_vocab(self, shingles: list[set]) -> tuple[dict[str,int],dict[int, str]]:
-        all_shingles = set().union(shingles)
+    def create_vocab(self, shingles: list[set[str]]) -> tuple[dict[str,int],dict[int, str]]:
+        all_shingles = set().union(*shingles)
         vocab = {}
         inv_vocab = {}
         for i,shingle in enumerate(all_shingles):
@@ -59,6 +65,54 @@ class LSH():
 
     
     
-    def create_minhash_order(self, vocab_size: int) -> list[int]:
-        minhash_order = random.choice()
+    def create_minhashes(self, vocab_size: int, signature_length: int) -> list[int]:
+        minhashes = []
+        for _ in range(signature_length):
+            minhash = list(range(vocab_size))
+            random.shuffle(minhash) 
+            minhashes.append(minhash)
+        return minhashes
     
+    def compress_to_signatures(self, one_hot_encoded_samples: list[list[int]]) -> list[list[int]]:
+        signatures = []  
+        
+        for sample in one_hot_encoded_samples:
+            signature = []
+            for minhash in self.minhashes:
+                for idx in minhash:
+                    if sample[idx] == 1:
+                        signature.append(idx)
+                        break
+            signatures.append(signature)
+
+        return signatures
+    
+    def create_bands_and_hash(self, signatures: list[list[int]]):
+        
+        # Create hash tables
+        hash_tables = [{} for _ in range(self.num_bands)]
+        
+        for doc_id, signature in enumerate(signatures):
+            # Split into bands and hash
+            for band_num in range(self.num_bands):
+                band = signature[band_num * self.band_size : (band_num + 1) * self.band_size]
+                # No actual hashing since it is quite redudndant for python
+                # Though we can hash the tuple here
+                key = tuple(band)
+                
+                # Store in hash table
+                hash_tables[band_num].setdefault(key, []).append(doc_id) 
+        return hash_tables
+                
+    def find_candidates(self):
+        candidates = set()
+        
+        for hash_table in self.hash_tables:
+            for bucket in hash_table.values():
+                # If more than one band hash is in the hash table -> match
+                if len(bucket) > 1:  
+                    for i in range(len(bucket)):
+                        for j in range(i + 1, len(bucket)):
+                            candidates.add(tuple(sorted([bucket[i], bucket[j]])))
+        
+        return candidates
